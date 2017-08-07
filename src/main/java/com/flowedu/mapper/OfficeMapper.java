5491832ef32721b47d607a6ea176fbf6a85973f6
ji ho An <anjo0080@gmail.com>
2017-08-07 11:24:10 +0900

    학원정보 입력 기능 초기 구성.

---
+++ src/main/java/com/flowedu/mapper/OfficeMapper.java
@@ -0,0 +1,12 @@
+package com.flowedu.mapper;
+
+import com.flowedu.dto.OfficeDto;
+
+/**
+ * Created by jihoan on 2017. 8. 7..
+ */
+public interface OfficeMapper {
+
+    /** INSERT **/
+    void saveAcademy(OfficeDto officeDto);
+}
