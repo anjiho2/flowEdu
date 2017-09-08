138549626970ff1c948a06c884861296ee07bb47
ji ho An <anjo0080@gmail.com>
2017-09-08 16:33:13 +0900

    .

---
+++ src/main/java/com/flowedu/define/datasource/StudentMemoType.java
@@ -0,0 +1,35 @@
+package com.flowedu.define.datasource;
+
+/**
+ * Created by jihoan on 2017. 9. 8..
+ */
+public enum StudentMemoType {
+    REG(0, "등록"),
+    NORAML(1, "일반"),
+    STUDY(2, "학습"),
+    PERIOD(3, "정기");
+
+    int studentMemoTypceCode;
+
+    String studentMemoTypeName;
+
+    StudentMemoType(int studentMemoTypceCode, String studentMemoTypeName) {
+        this.studentMemoTypceCode = studentMemoTypceCode;
+        this.studentMemoTypeName = studentMemoTypeName;
+    }
+
+    public static StudentMemoType getStudentMemoTypeCode(int studentMemoTypceCode) {
+        for (StudentMemoType type : StudentMemoType.values()) {
+            if (type.studentMemoTypceCode == studentMemoTypceCode) {
+                return type;
+            }
+        }
+        return null;
+    }
+
+    public static String getStudentMemoTypeName(int studentMemoTypceCode) {
+        return null;
+    }
+
+
+}
