1954456a6aa3e1cf43cbe5c443b9662d61e4a7d2
ji ho An <anjo0080@gmail.com>
2018-01-16 14:11:15 +0900

    비밀번호 랜덤 양식 변경.

--- src/main/java/com/flowedu/util/RandomMake.java
+++ src/main/java/com/flowedu/util/RandomMake.java
@@ -116,7 +116,7 @@ public class RandomMake {
 	 public static String getRandomPassword() {
 		 String[] str = {"a","b","c","d","e","f","g","h","i","j","k",
 				 "l","m","n","o","p","q","r","s","t","u","v",
-				 "w","x","y","z","0","1","2","3","4","5","6","7","8","9"};
+				 "w","x","y","z"};
 		 StringBuffer sb = new StringBuffer();
 		 Random rn = new Random();
 		 for (int i = 0; i < 8; i++) {
